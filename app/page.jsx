8f12cbe0e5e47833036c63ccbc6573961bc16833
gabu322 <105678876+gabu322@users.noreply.github.com>
2024-12-21 07:22:37 +0000

    Small general fixing

--- app/page.jsx
+++ app/page.jsx
@@ -12,13 +12,15 @@ export default function Home() {
    const [issuers, setIssuers] = useState([]);
 
    useEffect(() => {
-      axios.get('/api/cards')
-         .then((response) => setCardList(response.data))
-         .catch((err) => console.error(err));
-
-      axios.get('/api/issuers')
-         .then((response) => setIssuers(response.data))
-         .catch((err) => console.error(err));
+      try {
+         axios.get('/api/cards')
+            .then((response) => setCardList(response.data))
+         axios.get('/api/issuers')
+            .then((response) => setIssuers(response.data))
+      } catch (error) {
+         console.log("error");
+         console.error(error);
+      }
    }, []);
 
    return <main className="min-h-screen flex flex-col gap-5">
@@ -43,7 +45,7 @@ export function Card({
 
    const lightenedColor = lighten(0.1, issuer.color);
    const darkenedColor = darken(0.1, issuer.color);
-   n
+
    return <div
       className={`w-full aspect-[8/5] p-3 rounded-lg flex flex-col ${className || ''} text-white `}
       style={{ background: `linear-gradient(to bottom, ${darkenedColor}, ${lightenedColor})` }}
@@ -55,21 +57,19 @@ export function Card({
       </div>
 
       <div className="flex-grow flex flex-col justify-between">
-         {card.acceptsDebit ? <h3 className="leading-none">$ {card.DebitCard.balance}</h3> : <div />}
+         {card.debit ? <h3 className="leading-none">$ {card.debitCard.balance}</h3> : <div />}
 
-         {card.acceptsCredit && (
-            <div className="flex flex-col mb-1">
-               <h6 className="text-gray-300 leading-none">
-                  Used credit: (of ${card.CreditCard?.creditLimit} limit)
-               </h6>
-               <h3>$ {card.CreditCard?.currentCredit}</h3>
-            </div>
-         )}
+         {card.credit && <div className="flex flex-col mb-1">
+            <h6 className="text-gray-300 leading-none">
+               Used credit: (of ${card.creditCard?.creditLimit} limit)
+            </h6>
+            <h3>$ {card.creditCard?.currentCredit}</h3>
+         </div>}
       </div>
       <div className="w-full flex flex-row justify-between">
          <div>{'**** **** **** ' + card.number.slice(-4)}</div>
 
-         <div className="flex" onClick={() => router.push("/cards/" + card.id)}>
+         <div className="flex cursor-pointer" onClick={() => router.push("/cards/" + card.id)}>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
                <circle cx="10" cy="10" r="4" />
             </svg>

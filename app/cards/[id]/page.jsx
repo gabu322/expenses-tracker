e6e9e556d8358fff8052d2e8000331d8a2e04e4b
gabu322 <105678876+gabu322@users.noreply.github.com>
2025-01-06 23:27:50 +0000

    Added card visualization page

--- app/cards/[id]/page.jsx
+++ app/cards/[id]/page.jsx
@@ -1,19 +1,69 @@
-import Button from "@/components/Button";
-import { use } from "react";
+'use client';
 
-export default async function Page({ params }) {
-   const { id } = await params;
+import { use, useEffect, useState } from "react";
+import axios from "axios";
 
-   return <main>
-      <div className="w-full h-24 grid grid-cols-2 gap-4">
-         <div className="bg-white shadow rounded">
+import IDollar from "@/public/icons/dynamic/dollar";
+import CountUp from "react-countup";
 
-         </div>
-         <div className="bg-white shadow rounded">
+export default function Page({ params }) {
+   const { id } = use(params);
+   const [card, setCard] = useState();
 
-         </div>
+   useEffect(() => {
+      axios.get(`/api/cards/${id}`)
+         .then((response) => {
+            setCard(response.data)
+            console.log(response.data)
+         })
+         .catch(console.error);
+   }, []);
+
+   return <main className="flex flex-col gap-4">
+      <h2>{card?.name}</h2>
+
+      <div className="w-full grid grid-cols-2 gap-4">
+         {/* Debit card info */}
+         {card?.debit && <>
+            <div className="card h-16 flex-row-4">
+               <IDollar className="h-10 w-10 p-1 bg-lime-200 rounded-md" color="darkgreen" />
+               <div className="flex flex-col justify-around">
+                  <span className="font-light">Balance</span>
+
+                  <CountUp className="font-bold" prefix="$" end={card?.debitCard.balance} decimals={2} />
+               </div>
+            </div>
+            <div className="card h-16 flex-row-4" style={{ backgroundColor: card?.debitCard.balance < 0 ? 'rgba(255, 0, 0, 0.1)' : 'rgba(0, 255, 0, ll0.1)' }}>
+               <IDollar className="h-10 w-10 p-1 bg-gray-300 rounded-md" color="gray" />
+
+               {/* <div className="flex flex-col justify-around">
+                  <span>Spent credit</span>
+
+                  <span>${parseFloat(card?.creditCard.currentCredit).toFixed(2)}</span>
+               </div> */}
+            </div>
+         </>}
+
+         {/* Credit card info */}
+         {card?.credit && <>
+            <div className="card h-16 flex-row-4">
+               <IDollar className="h-10 w-10 p-1 bg-lime-200 rounded-md" color="darkgreen" />
+               <div className="flex flex-col justify-around">
+                  <span className="font-light">Credit Limit</span>
+
+                  <span className="font-bold">${parseFloat(card?.creditCard.creditLimit).toFixed(2)}</span>
+               </div>
+            </div>
+            <div className="card h-16 flex-row-4">
+               <IDollar className="h-10 w-10 p-1 bg-red-200 rounded-md" color="red" />
+
+               <div className="flex flex-col justify-around">
+                  <span>Spent credit</span>
+
+                  <span className="font-bold">${parseFloat(card?.creditCard.currentCredit).toFixed(2)}</span>
+               </div>
+            </div>
+         </>}
       </div>
-      {id}
    </main>;
 };
-

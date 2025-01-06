b16bb5211d1153b382c26707d8ee004fc04697a3
gabu322 <105678876+gabu322@users.noreply.github.com>
2025-01-06 23:26:04 +0000

    Added temporary login page

--- app/user/page.jsx
+++ app/user/page.jsx
@@ -2,12 +2,13 @@ import Button from "@/components/Button";
 import Input from "@/components/Input";
 
 export default function Page({ params }) {
-   return <main className="flex flex-col justify-center items-center h-screen">
+   return <main className="flex flex-col justify-center items-center h-screen w-full gap-4">
 
       <h1 className="text-2xl">Login</h1>
 
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4 w-4/5">
          <Input
+            className="w-full"
             name="email"
             label="Email"
             type="email"
@@ -15,18 +16,25 @@ export default function Page({ params }) {
          />
 
          <Input
+            className="w-full"
             name="password"
             label="Senha"
             type="password"
             required
          />
 
-         <Button type="submit">Entrar</Button>
-
+         <Button
+            className="w-full"
+            type="submit"
+            text={"Entrar"}
+         />
          <hr />
 
          <p className="text-left">Ainda não possui cadastro?</p>
-         <Button type="button">Cadastrar</Button>
+         <Button
+            className="w-full"
+            text={"Cadastrar"}
+         />
       </form>
    </main>;
 };

4acf6d52a8c1ccf6a37807cb77f12a86154026b7
gabu322 <105678876+gabu322@users.noreply.github.com>
2025-01-07 07:53:10 +0000

    Created route to make it easier to edit the database WHILE DEVELOPING

---
+++ app/api/clearDatabase/route.js
@@ -0,0 +1,18 @@
+// clear the database
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET(req, res) {
+   try {
+      await prisma.creditCard.deleteMany();
+      await prisma.debitCard.deleteMany();
+      await prisma.transaction.deleteMany();
+      await prisma.card.deleteMany();
+      await prisma.issuer.deleteMany();
+      // await prisma.session.deleteMany();
+      await prisma.user.deleteMany();
+      return NextResponse.json({ message: "Database cleared" }, { status: 200 });
+   } catch (error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+   }
+}

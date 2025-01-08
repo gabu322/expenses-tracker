253b7a5fd29ba574ee260a0874009840cdebc040
gabu322 <105678876+gabu322@users.noreply.github.com>
2025-01-08 01:00:08 +0000

    Fixed small bug that appeared with the new folder tree

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -11,7 +11,7 @@ export default function Navbar() {
    const [isOpen, setIsOpen] = useState(false);
    const toggleNavbar = () => setIsOpen(!isOpen);
 
-   return <div className={`w-full h-96 fixed bg-white z-10 transition-all duration-500 ${isOpen ? "-bottom-0" : "-bottom-80"}`}>
+   return <div className={`w-full h-96 fixed bg-white z-10 transition-all duration-500 left-0 ${isOpen ? "-bottom-0" : "-bottom-80"}`}>
       <Overlay isOpen={isOpen} toggleNavbar={toggleNavbar} />
 
       <Transaction isOpen={isOpen} toggleNavbar={toggleNavbar} />

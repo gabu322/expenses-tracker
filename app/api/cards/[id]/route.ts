5455f0e132ab671659486342dc943d93fb0938a6
gabu322 <105678876+gabu322@users.noreply.github.com>
2025-02-22 02:01:16 +0000

    refactor(api/cards): standardize and simplify [id] route implementation

--- app/api/cards/[id]/route.ts
+++ app/api/cards/[id]/route.ts
@@ -5,91 +5,63 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
 import { getIdFromRequest } from "@/utils/functions/api/getIdFromRequest";
 
-export async function GET(req: NextRequest) {
-   const { id, error } = getIdFromRequest(req);
-   if (error) return error;
-
-   try {
-      const card = await prisma.card.findUnique({
-         where: {
-            id: Number(id),
-         },
-         include: {
-            creditCard: true,
-            debitCard: true,
-         },
-      });
-
-      return NextResponse.json(card, { status: 200 });
-   } catch (error: any) {
-      return NextResponse.json({ error: error.message }, { status: 500 });
-   }
-}
-
-export async function PUT(req: NextRequest) {
+export async function handler(req: NextRequest) {
    const { id, error } = getIdFromRequest(req);
    if (error) return error;
 
    try {
       const session = await getServerSession(authOptions);
+      if (!session) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
 
-      if (!session || !session.user) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+      const card = await prisma.card.findUnique({ where: { id } });
+      if (!card) return NextResponse.json({ error: "Card not found" }, { status: 404 });
 
-      const data = await req.json();
-      const cardData = updateCardSchema.parse(data);
+      if (card.userId !== session.user.id) return NextResponse.json({ error: "Card does not belong to user" }, { status: 401 });
 
-      if (cardData.userId !== session.user.id) throw new Error("Card does not belong to user");
+      switch (req.method) {
+         case "GET":
+            return NextResponse.json(card, { status: 200 });
 
-      const updatedCard = await prisma.card.update({
-         where: {
-            id: Number(id),
-         },
-         data: {
-            nickname: cardData.nickname,
-            number: cardData.number,
-            expiration: cardData.expiration,
-            cvv: cardData.cvv,
-            // userId: 1, // Hardcoded for now
+         case "PUT":
+            const requestData = await req.json();
+            const updatedCardData = updateCardSchema.parse(requestData);
 
-            // Used like this for consistency
-            issuer: { connect: { id: cardData.issuerId } },
+            const updatedCard = await prisma.card.update({
+               where: { id },
+               data: {
+                  nickname: updatedCardData.nickname,
+                  number: updatedCardData.number,
+                  expiration: updatedCardData.expiration,
+                  cvv: updatedCardData.cvv,
+                  issuer: { connect: { id: updatedCardData.issuerId } },
+                  credit: updatedCardData.credit,
+                  creditCard: updatedCardData.credit
+                     ? {
+                          update: {
+                             creditLimit: updatedCardData.creditLimit,
+                             currentCredit: updatedCardData.currentCredit || 0.0,
+                          },
+                       }
+                     : undefined,
+                  debit: updatedCardData.debit,
+                  debitCard: updatedCardData.debit ? { update: { balance: updatedCardData.balance } } : undefined,
+               },
+            });
 
-            credit: cardData.credit,
-            creditCard: cardData.credit
-               ? {
-                    update: {
-                       creditLimit: cardData.creditLimit,
-                       currentCredit: cardData.currentCredit || 0.0,
-                    },
-                 }
-               : undefined,
+            return NextResponse.json(updatedCard, { status: 200 });
 
-            debit: cardData.debit,
-            debitCard: cardData.debit ? { update: { balance: cardData.balance } } : undefined,
-         },
-      });
+         case "DELETE":
+            await prisma.card.delete({
+               where: { id },
+            });
 
-      return NextResponse.json(updatedCard, { status: 200 });
+            return NextResponse.json({ message: "Card deleted" }, { status: 200 });
+         default:
+            return NextResponse.json({ error: "Method Not Allowed" }, { status: 405 });
+      }
    } catch (error: any) {
-      if (error.name === "ZodError") return NextResponse.json({ error: error.errors }, { status: 400 });
+      if (error.name === "ZodError") return NextResponse.json({ errors: error.errors }, { status: 400 });
 
-      return NextResponse.json({ error: error.message }, { status: 500 });
-   }
-}
-
-export async function DELETE(req: NextRequest) {
-   const { id, error } = getIdFromRequest(req);
-   if (error) return error;
-
-   try {
-      await prisma.card.delete({
-         where: {
-            id,
-         },
-      });
-
-      return NextResponse.json({ message: "Card deleted" }, { status: 200 });
-   } catch (error: any) {
       return NextResponse.json({ error: error.message }, { status: 500 });
    }
 }

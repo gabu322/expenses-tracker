b712d5b436bbf957f79f5d5ab8db9265dc6e085f
gabu322 <gabrieldallorto@hotmail.com>
2025-02-24 20:00:36 -0300

    fix(api/user): changed old function that got the id to getParams

--- app/api/user/[id]/route.ts
+++ app/api/user/[id]/route.ts
@@ -1,64 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
-import { getIdFromRequest } from "@/utils/functions/api/getIdFromRequest";
+import { getParams, ParamsType } from "@/utils/params";
 
-export async function GET(req: NextRequest) {
-   const { id, error } = getIdFromRequest(req);
-   if (error) return error;
-
-   try {
-      const user = await prisma.user.findUnique({ where: { id } });
-
-      if (!user) return NextResponse.json({ error: "User not found" }, { status: 404 });
-
-      return NextResponse.json(user, { status: 200 });
-   } catch (error: any) {
-      return NextResponse.json({ error: error.message }, { status: 500 });
-   }
-}
-
-export async function DELETE(req: NextRequest) {
-   const { id, error } = getIdFromRequest(req);
-   if (error) return error;
+async function handler(req: NextRequest, context: ParamsType) {
+   const { id } = await getParams(context);
 
    try {
       const user = await prisma.user.findUnique({ where: { id } });
 
       if (!user) return NextResponse.json({ error: "User not found" }, { status: 404 });
 
-      await prisma.user.delete({ where: { id } });
+      switch (req.method) {
+         case "GET":
+            return NextResponse.json(user, { status: 200 });
 
-      return NextResponse.json({ message: "User deleted" }, { status: 200 });
+         case "DELETE":
+            await prisma.user.delete({ where: { id } });
+
+            return NextResponse.json({ message: "User deleted" }, { status: 200 });
+
+         default:
+            return NextResponse.json({ error: "Method Not Allowed" }, { status: 405 });
+      }
    } catch (error: any) {
       return NextResponse.json({ error: error.message }, { status: 500 });
    }
 }
-
-// * Alternative way to export the functions
-// export async function handler(req: NextRequest) {
-//   const { id, error } = getIdFromRequest(req);
-//   if (error) return error;
-
-//    try {
-//       const user = await prisma.user.findUnique({ where: { id } });
-
-//       if (!user) return NextResponse.json({ error: "User not found" }, { status: 404 });
-
-//       switch (req.method) {
-//          case "GET": // * GET
-
-//             return NextResponse.json(user, { status: 200 });
-//          case "DELETE": // * DELETE
-
-//             await prisma.user.delete({ where: { id } });
-
-//             return NextResponse.json({ message: "User deleted" }, { status: 200 });
-//          default:
-//             return NextResponse.json({ error: "Method Not Allowed" }, { status: 405 });
-//       }
-//    } catch (error: any) {
-//       return NextResponse.json({ error: error.message }, { status: 500 });
-//    }
-// }
-
-// export { handler as GET, handler as DELETE };

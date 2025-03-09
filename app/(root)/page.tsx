7a267850b8fcbb2fb5e063e00fc9049fddb092e8
gabu322 <gabrieldallorto@hotmail.com>
2025-03-09 20:08:57 -0300

    fead(app): add loading on cards page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,33 +7,52 @@ import Button from "@/components/Button";
 import { IssuerType } from "@/utils/types/index";
 import { useCards } from "./CardContext";
 import { Card } from "@/components/pages/Card";
+import { toast } from "react-toastify";
+import Skeleton from "@/components/Skeleton";
 
 export default function Home() {
    const { cards } = useCards();
    const [issuers, setIssuers] = useState<IssuerType[]>([]);
+   const [loading, setLoading] = useState<boolean>(true);
 
    useEffect(() => {
-      try {
-         axios.get("/api/issuers").then((response) => setIssuers(response.data));
-      } catch (error) {
-         console.error("Failed to fetch issuers:", error);
-      }
+      const fetchIssuers = async () => {
+         try {
+            const response = await axios.get("/api/issuers");
+            setIssuers(response.data);
+         } catch (error) {
+            console.error("Failed to fetch issuers:", error);
+         } finally {
+            setLoading(false);
+         }
+      };
+
+      fetchIssuers();
    }, []);
 
    return (
-      <div className="flex flex-col gap-5">
+      <div className="flex flex-col gap-4">
          <div className="flex flex-row justify-between">
             <h1>Seus cartões</h1>
             <Button href={"/cards"}>Novo Cartão</Button>
+            <Button onClick={() => toast("Hello, world!")}>Toast</Button>
          </div>
 
-         {cards.map((card) => (
-            <Card
-               key={card.id}
-               card={card}
-               issuer={issuers.find((issuer) => issuer.id === card.issuerId)}
-            />
-         ))}
+         {loading ? (
+            <>
+               <Skeleton className="aspect-[8/5] rounded-lg shadow-lg max-w-[400px]" />
+               <Skeleton className="aspect-[8/5] rounded-lg shadow-lg max-w-[400px]" />
+               <Skeleton className="aspect-[8/5] rounded-lg shadow-lg max-w-[400px]" />
+            </>
+         ) : (
+            cards.map((card) => (
+               <Card
+                  key={card.id}
+                  card={card}
+                  issuer={issuers.find((issuer) => issuer.id === card.issuerId)}
+               />
+            ))
+         )}
       </div>
    );
 }

156498f0fc5fd03f2384c99be8eb7fab08050da4
gabu322 <gabrieldallorto@hotmail.com>
2025-03-09 20:06:42 -0300

    feat(components): add skeleton component for loading

---
+++ components/Skeleton.tsx
@@ -0,0 +1,8 @@
+export default function Skeleton({ className = "", ...props }) {
+   return (
+      <div
+         className={`bg-slate-300 animate-pulse rounded ${className}`}
+         {...props}
+      />
+   );
+}

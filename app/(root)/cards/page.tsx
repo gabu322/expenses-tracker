7e362ab99fa3363779e1101f374f0dc3208af39c
gabu322 <gabrieldallorto@hotmail.com>
2025-03-11 03:36:38 -0300

    style(newCard): update form

--- app/(root)/cards/page.tsx
+++ app/(root)/cards/page.tsx
@@ -59,115 +59,112 @@ export default function Page() {
    };
 
    return (
-      <div className="flex flex-col gap-4">
-         <h1 className="">Criando novo cartão</h1>
+      <form
+         className="flex flex-col gap-4"
+         onSubmit={handleSubmit}
+      >
+         <div className="flex flex-col gap-4 bg-white rounded-lg p-4 shadow-md">
+            <h1 className="">Criando novo cartão</h1>
 
-         <form
-            className="flex flex-col gap-6"
-            onSubmit={handleSubmit}
-         >
-            <div className="flex flex-col gap-4">
-               <h2>Informações gerais</h2>
-               <Input
-                  name="nickname"
-                  label="Nome do cartão"
-                  onChange={handleChange}
-                  required
-               />
+            <Input
+               name="nickname"
+               label="Nome do cartão"
+               onChange={handleChange}
+               required
+            />
 
-               <Select
-                  name="issuerId"
-                  label="Banco"
-                  options={issuers.filter((issuer) => issuer.id).map((issuer) => ({ value: issuer.id as string, text: issuer.name }))}
-                  onChange={handleChange}
-                  required
-               />
+            <Select
+               name="issuerId"
+               label="Banco"
+               options={issuers.filter((issuer) => issuer.id).map((issuer) => ({ value: issuer.id as string, text: issuer.name }))}
+               onChange={handleChange}
+               required
+            />
 
-               <Input
-                  name="number"
-                  label="Número do cartão"
-                  onChange={handleChange}
-                  mask="**** **** **** 0000"
-                  initialValue="**** **** **** "
-                  required
-               />
+            <Input
+               name="number"
+               label="Número do cartão"
+               onChange={handleChange}
+               mask="**** **** **** 0000"
+               initialValue="**** **** **** "
+               required
+            />
 
-               <Input
-                  name="expiration"
-                  label="Data de expiração"
-                  onChange={handleChange}
-                  type="month"
-               />
+            <Input
+               name="expiration"
+               label="Data de expiração"
+               onChange={handleChange}
+               type="month"
+            />
 
-               <Input
-                  name="cvv"
-                  label="CVV"
-                  onChange={handleChange}
-                  mask="000"
-               />
+            <Input
+               name="cvv"
+               label="CVV"
+               onChange={handleChange}
+               mask="000"
+            />
 
-               <div className="flex justify-between">
-                  <div>Tipo do cartão</div>
+            <div className="flex justify-between">
+               <div>Tipo do cartão</div>
 
-                  <div className="flex gap-4">
-                     <Checkbox
-                        name="credit"
-                        onChange={handleCheckboxChange}
-                        label={"Crédito"}
-                     />
+               <div className="flex gap-4">
+                  <Checkbox
+                     name="credit"
+                     onChange={handleCheckboxChange}
+                     label={"Crédito"}
+                  />
 
-                     <Checkbox
-                        name="debit"
-                        onChange={handleCheckboxChange}
-                        label="Débito"
-                     />
-                  </div>
+                  <Checkbox
+                     name="debit"
+                     onChange={handleCheckboxChange}
+                     label="Débito"
+                  />
                </div>
             </div>
+         </div>
 
-            {cardData.credit && (
-               <div className="flex flex-col gap-4">
-                  <h2>Informações de crédito</h2>
+         {cardData.credit && (
+            <div className="flex flex-col gap-4 bg-white p-4 rounded-lg shadow-md">
+               <h2>Informações de crédito</h2>
 
-                  <Input
-                     name="limit"
-                     label="Limite de crédito"
-                     onChange={handleChange}
-                     currency={"R$"}
-                     required={cardData.credit}
-                  />
+               <Input
+                  name="limit"
+                  label="Limite de crédito"
+                  onChange={handleChange}
+                  currency={"R$"}
+                  required={cardData.credit}
+               />
 
-                  <Input
-                     name="usedLimit"
-                     label="Crédito atual"
-                     onChange={handleChange}
-                     currency={"R$"}
-                     required={cardData.credit}
-                  />
-               </div>
-            )}
+               <Input
+                  name="usedLimit"
+                  label="Crédito atual"
+                  onChange={handleChange}
+                  currency={"R$"}
+                  required={cardData.credit}
+               />
+            </div>
+         )}
 
-            {cardData.debit && (
-               <div className="flex flex-col gap-4">
-                  <h2>Informações de débito</h2>
+         {cardData.debit && (
+            <div className="flex flex-col gap-4 bg-white rounded-lg p-4 shadow-md">
+               <h2>Informações de débito</h2>
 
-                  <Input
-                     name="balance"
-                     label="Saldo da conta"
-                     onChange={handleChange}
-                     currency={"R$"}
-                     required={cardData.debit}
-                  />
-               </div>
-            )}
+               <Input
+                  name="balance"
+                  label="Saldo da conta"
+                  onChange={handleChange}
+                  currency={"R$"}
+                  required={cardData.debit}
+               />
+            </div>
+         )}
 
-            <Button
-               type={"submit"}
-               className={"text-white"}
-            >
-               Adicionar
-            </Button>
-         </form>
-      </div>
+         <Button
+            type={"submit"}
+            className={"text-white"}
+         >
+            Adicionar
+         </Button>
+      </form>
    );
 }

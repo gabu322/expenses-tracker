ab4d1b6ac5a3eddc08efbf993d538d037314b702
gabu322 <gabrieldallorto@hotmail.com>
2025-03-17 02:55:17 -0300

    feat(toast): add function to handle errors with notifications

---
+++ lib/functions/handleToastError.ts
@@ -0,0 +1,19 @@
+import { Id, toast } from "react-toastify";
+
+export function handleToastError(error: unknown, toastId: Id) {
+   let errorMessage = "Ocorreu um erro inesperado.";
+
+   if (typeof error === "object" && error !== null) {
+      const err = error as { response?: { data?: { error?: string; errors?: { message: string }[] } }; message?: string };
+
+      if (err.response?.data?.error) {
+         errorMessage = err.response.data.error;
+      } else if (err.response?.data?.errors?.length) {
+         errorMessage = err.response.data.errors[0].message;
+      } else if (err.message) {
+         errorMessage = err.message;
+      }
+   }
+
+   toast.update(toastId, { render: errorMessage, type: "error", isLoading: false, autoClose: 3000 });
+}
